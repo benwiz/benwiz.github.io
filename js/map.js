421c3a9996dfc0dbff587d35bb96d6fe867542d4
Ben Wisialowski <ben@wiz3.com>
2017-04-23 16:32:51 -0500

    clean up dots a bit

--- js/map.js
+++ js/map.js
@@ -11,38 +11,66 @@ var map = new Datamap({
         popupOnHover: false,
         highlightOnHover: false
     },
+    bubblesConfig: {
+        borderWidth: 0,
+        // borderOpacity: 1,
+        // borderColor: '#FFFFFF',
+        // popupOnHover: true,
+        // radius: null,
+        // popupTemplate: function(geography, data) {
+        //   return '<div class="hoverinfo"><strong>' + data.name + '</strong></div>';
+        // },
+        // fillOpacity: 0.75,
+        // animate: true,
+        // highlightOnHover: true,
+        highlightFillColor: 'cyan',
+        // highlightBorderColor: 'rgba(250, 15, 160, 0.2)',
+        // highlightBorderWidth: 2,
+        // highlightBorderOpacity: 1,
+        // highlightFillOpacity: 0.85,
+        // exitDelay: 100,
+        // key: JSON.stringify
+    },
     fills: {
         defaultFill: '#444444',
-        USA: 'blue',
-        RUS: 'red'
+        light: '#FFFFFF',
+        dark: '#777777'
     }
 });
 
 map.bubbles([
     {
-        name: 'Not a bomb, but centered on Brazil',
+        name: 'Brazil',
         radius: 5,
-        fillKey: 'USA',
+        fillKey: 'light',
         centered: 'BRA'
     },
     {
-        name: 'Not a bomb',
-        radius: 5,
-        fillKey: 'USA',
+        name: 'New Orleans',
+        radius: 10,
+        fillKey: 'light',
         latitude: 29.926596,
         longitude: -90.081299
     },
     {
-        name: 'Castle Bravo',
+        name: 'idk1',
         radius: 5,
+        fillKey: 'light',
         latitude: 11.415,
         longitude: 165.1619
-    },{
-        name: 'Tsar Bomba',
-        radius: 10,
-        fillKey: 'RUS',
+    },
+    {
+        name: 'idk2',
+        radius: 5,
+        fillKey: 'light',
         latitude: 73.482,
         longitude: 54.5854
+    },
+    {
+        name: 'idk3',
+        radius: 5,
+        fillKey: 'dark',
+        centered: 'USA'
     }
 ], {
     popupTemplate: function(geo, data) {
