fa33a4574fe5116437a833b6d6c7ced846ab7d8c
Ben Wisialowski <bwisialowski@gmail.com>
2017-12-20 00:13:57 -0600

    move unused code

--- dialog.js
+++
@@ -1,9 +0,0 @@
-var dialog = new mdc.dialog.MDCDialog(document.querySelector('#mdc-dialog-default'));
-
-dialog.listen('MDCDialog:accept', function() {
-  console.log('accepted');
-})
-
-dialog.listen('MDCDialog:cancel', function() {
-  console.log('canceled');
-})
