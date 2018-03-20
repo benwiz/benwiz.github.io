27bcfbc57e480cc4ee20de5f72bbe88d6f58ec0a
Ben Wisialowski <bwisialowski@gmail.com>
2018-03-19 23:04:00 -0500

    sort out width

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import {MDCRipple} from '@material/ripple';
-
-// Apply ripple to all ripple-buttons
-const rippleButtons = document.querySelectorAll('.ripple-button');
-rippleButtons.forEach(function (button) {
-  new MDCRipple(button);
-});
+// // All ripples
+// import {MDCRipple} from '@material/ripple';
+// // Apply ripple to all mdc-ripple-surfaces
+// document.querySelectorAll('.mdc-ripple-surface').forEach(function (surface) {
+//   MDCRipple.attachTo(surface);
+//   console.log(surface);
+// });
