a7a73700d2521933247eaab53a95beb0aece6816
Ben Wisialowski <bwisialowski@gmail.com>
2018-03-19 21:21:11 -0500

    js

--- README.md
+++ README.md
@@ -1,11 +1,27 @@
 # benwiz.github.io
 
+## Notes
+
+Got set up using [getting started with material design](https://material.io/components/web/docs/getting-started/). Needed to specify `extract-loader@1`.
+
 ## Useful commands
 
 Start local server.
 
 ```bash
 npm start
+```
+
+Start local server that better simulates what GitHub Pages does.
+
+```bash
+http-server
+```
+
+To build updates
+
+```bash
+npx webpack
 ```
 
 ## To Do
--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,57 @@
 const glob = require('glob');
 const path = require('path');
 
-module.exports = [{
-  entry: './app.scss',
-  output: {
-    // This is necessary for webpack to compile
-    // But we never use style-bundle.js
-    filename: 'style-bundle.js',
-  },
-  module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: 'bundle.css',
+module.exports = [
+  {
+    entry: './app.scss',
+    output: {
+      // This is necessary for webpack to compile
+      // But we never use style-bundle.js
+      filename: 'style-bundle.js',
+    },
+    module: {
+      rules: [{
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'bundle.css',
+            },
           },
-        },
-        { loader: 'extract-loader' },
-        { loader: 'css-loader' },
-        { loader: 'sass-loader',
-          options: {
-            // includePaths: glob.sync('node_modules').map((d) => path.join(__dirname, d))
-            // Tutorial says to use importer even though includePaths appears to work.
-            importer: function(url, prev) {
-              if(url.indexOf('@material') === 0) {
-                var filePath = url.split('@material')[1];
-                var nodeModulePath = `./node_modules/@material/${filePath}`;
-                return { file: require('path').resolve(nodeModulePath) };
+          { loader: 'extract-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader',
+            options: {
+              // includePaths: glob.sync('node_modules').map((d) => path.join(__dirname, d))
+              // Tutorial says to use importer even though includePaths appears to work.
+              importer: function(url, prev) {
+                if(url.indexOf('@material') === 0) {
+                  var filePath = url.split('@material')[1];
+                  var nodeModulePath = `./node_modules/@material/${filePath}`;
+                  return { file: require('path').resolve(nodeModulePath) };
+                }
+                return { file: url };
               }
-              return { file: url };
-            }
+            },
           },
-        },
-      ]
-    }]
+        ]
+      }]
+    },
   },
-}];
-console.log(glob.sync('node_modules').map((d) => path.join(__dirname, d)));
+  {
+    entry: "./app.js",
+    output: {
+      filename: "bundle.js"
+    },
+    module: {
+      loaders: [{
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }]
+    },
+  },
+];
